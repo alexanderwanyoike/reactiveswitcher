9197a7b446353c253cf1260e74f2341174fb37dd
alexwanyoike <wanyoike.alexander@gmail.com>
2016-08-08 15:02:07 +0100

    Remove meteor dependency

---
+++ app/js/app.js
@@ -0,0 +1,21 @@
+$('body').ready(function () {
+    $.get("views", function (settings) {
+        currentIndex = 0;
+        console.log(settings["views"]);
+        $('#displayFrame').attr('src', settings['views'][currentIndex]);
+
+
+        setInterval(function () {
+            nextIndex = (currentIndex + 1) % settings.views.length;
+            currentIndex = nextIndex;
+            $.get("views", function (settings) {
+                currentView = settings.views[nextIndex];
+                $('#displayFrame').fadeOut('slow', function () {
+                    $(this).attr('src', currentView);
+                    $(this).fadeIn('slow');
+                });
+            });
+
+        }, settings.initalRenderDuration);
+    });
+});
